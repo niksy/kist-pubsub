87d62cef9d6759973125444912b88ecc80362409
Ivan Nikolić <niksy5@gmail.com>
2014-12-14 19:26:54 +0100

    Release 0.2.0

--- dist/kist-pubsub.js
+++ dist/kist-pubsub.js
@@ -1,4 +1,4 @@
-/*! kist-pubsub 0.1.1 - Simple publish/subscribe system. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT */
+/*! kist-pubsub 0.2.0 - Simple publish/subscribe system. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self);var n=f;n=n.jQuery||(n.jQuery={}),n=n.kist||(n.kist={}),n.PubSub=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 var $ = (typeof window !== "undefined" ? window.$ : typeof global !== "undefined" ? global.$ : null);
@@ -26,7 +26,10 @@ function getQueue ( topic, fn ) {
 	if ( this.options.queue ) {
 		if ( topic in this.queue ) {
 			data = this.queue[topic];
-			data.unshift(null);
+			if ( data[0] instanceof $.Event && data[0].type === 'kistpubsubqueue' ) {
+				data.shift();
+			}
+			data.unshift($.Event('kistpubsubqueue'));
 			fn.apply(this.o[0], data);
 		}
 	}
@@ -44,10 +47,10 @@ function resolveEvent ( fn, condition ) {
 	var event = this.options.event;
 	function wrapper () {
 		var args = (!event ? [].slice.call(arguments, 1) : arguments);
-		if ( Boolean(condition.apply(this, arguments)) ) {
+		if ( Boolean(condition.apply(this, args)) ) {
 			return fn.apply(this, args);
 		}
-		return $.noop;
+		return $.noop();
 	}
 	if ( !event ) {
 		wrapper.guid = fn.guid = fn.guid || $.guid++;
@@ -68,6 +71,20 @@ function generateTopic ( topic ) {
 		return topic + '.' + this.options.namespace;
 	}
 	return topic;
+}
+
+/**
+ * @return {Array|Object}
+ */
+function generateEventData () {
+	var data = [].slice.call(arguments, 1);
+
+	// If there is only 1 argument and that argument is either array or object
+	// return that argument, otherwise return constructed array
+	if ( data.length === 1 && ($.type(data[0]) === 'array' || $.type(data[0]) === 'object') ) {
+		return data[0];
+	}
+	return data;
 }
 
 /**
@@ -144,6 +161,7 @@ $.extend(PubSub.prototype, {
 	 */
 	publish: function ( topic, data ) {
 		topic = generateTopic.call(this, topic);
+		data = generateEventData.apply(this, arguments);
 		setQueue.call(this, topic, data);
 		this.o.trigger.call(this.o, topic, data);
 	},
